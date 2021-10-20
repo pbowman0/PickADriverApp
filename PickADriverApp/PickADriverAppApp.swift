7bd70c41118457c83b12c186ac84c406aade2a67
pbowman0 <paigeelisabethbowman@gmail.com>
2021-10-20 10:17:43 -0500

    Initial Commit

---
+++ PickADriverApp/PickADriverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickADriverAppApp.swift
+//  PickADriverApp
+//
+//  Created by Student on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PickADriverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
